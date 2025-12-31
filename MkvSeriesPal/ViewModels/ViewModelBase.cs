d158917f935729896d87b33bf28ac82bf7f912df
Damjan Kovačič <letecnez@gmail.com>
2025-12-31 11:29:18 +0100

    Blank Avalonia app

---
+++ MkvSeriesPal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MkvSeriesPal.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ MkvSeriesPal/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MkvSeriesPal.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
